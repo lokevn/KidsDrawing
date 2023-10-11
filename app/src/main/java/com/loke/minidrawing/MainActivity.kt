56509f0cce209372d361554809a75168f8686aae
lokevn <baoviet2105@gmail.com>
2023-10-12 00:54:51 +0700

    brush size selector dialog

--- app/src/main/java/com/loke/minidrawing/MainActivity.kt
+++ app/src/main/java/com/loke/minidrawing/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.loke.minidrawing
 
+import android.app.Dialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageButton
 
 class MainActivity : AppCompatActivity() {
     private var drawingView: Drawing? = null
@@ -11,6 +13,34 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         drawingView = findViewById(R.id.drawing_view)
-        drawingView?.setSizeforBrush(20f)
+        //drawingView?.setSizeforBrush(20f)
+
+        val btnBrushSelect = findViewById<ImageButton>(R.id.brush_selector)
+        btnBrushSelect.setOnClickListener() {
+            showBrushSizeDialog()
+        }
+    }
+
+    private fun showBrushSizeDialog() {
+        val brushDialog = Dialog(this)
+        brushDialog.setContentView(R.layout.diaglog_brush_size)
+        brushDialog.setTitle("Brush Size:")
+        val btnSmallStroke = brushDialog.findViewById<ImageButton>(R.id.ib_small_brush)
+        btnSmallStroke.setOnClickListener() {
+            drawingView?.setSizeforBrush(5f)
+            brushDialog.dismiss()
+        }
+        val btnMediumStroke = brushDialog.findViewById<ImageButton>(R.id.ib_medium_brush)
+        btnMediumStroke.setOnClickListener() {
+            drawingView?.setSizeforBrush(10f)
+            brushDialog.dismiss()
+        }
+        val btnLargeStroke = brushDialog.findViewById<ImageButton>(R.id.ib_large_brush)
+        btnLargeStroke.setOnClickListener() {
+            drawingView?.setSizeforBrush(20f)
+            brushDialog.dismiss()
+        }
+        brushDialog.show()
+
     }
 }
