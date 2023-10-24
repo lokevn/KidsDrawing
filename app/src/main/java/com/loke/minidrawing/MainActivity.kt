8ac0dba77f28719c8f7bbcd80972648d99c11a1d
loke pc <baoviet2105@gmail.com>
2023-10-24 18:39:42 +0700

    update permission photo

--- app/src/main/java/com/loke/minidrawing/MainActivity.kt
+++ app/src/main/java/com/loke/minidrawing/MainActivity.kt
@@ -1,18 +1,40 @@
 package com.loke.minidrawing
 
+import android.Manifest
+import android.app.AlertDialog
 import android.app.Dialog
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.ImageButton
 import android.widget.LinearLayout
+import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.content.ContextCompat
 import androidx.core.view.get
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
     private var drawingView: Drawing? = null
+    private var mGalleryButton:ImageButton? = null
     private var mImageButtonCurrentPaint:ImageButton? = null
+    private val permResultLauncher:ActivityResultLauncher<Array<String>> =
+        registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()){
+            permissions ->
+            permissions.entries.forEach {
+                val perName = it.key
+                val isGranted = it.value
+                if(isGranted) {
+                    Toast.makeText(this, "Permission granted for {$perName}",Toast.LENGTH_SHORT).show()
+                }
+                else {
+                    Toast.makeText(this, "permission denied for {$perName}}", Toast.LENGTH_SHORT).show()
+                }
+            }
 
+        }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -29,6 +51,24 @@ class MainActivity : AppCompatActivity() {
         val btnBrushSelect = findViewById<ImageButton>(R.id.brush_selector)
         btnBrushSelect.setOnClickListener() {
             showBrushSizeDialog()
+        }
+
+        mGalleryButton = findViewById(R.id.bạckground_selector)
+        mGalleryButton?.setOnClickListener() {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M &&
+                shouldShowRequestPermissionRationale(Manifest.permission.ACCESS_FINE_LOCATION) &&
+                shouldShowRequestPermissionRationale(Manifest.permission.CAMERA)) {
+                AlertDialog.Builder(this).setTitle("Paint requires Storage&Location Permision")
+                    .setMessage("Paint not function properly without Storage&Camera access")
+                    .setPositiveButton("Cancel"){
+                    dialog, which-> dialog.dismiss()
+                }.create().show()
+            }
+            else {
+                permResultLauncher.launch(arrayOf(Manifest.permission.ACCESS_FINE_LOCATION,
+                    Manifest.permission.CAMERA))
+            }
+            //Snackbar.make(it, "you have clicked Gallery", Snackbar.LENGTH_LONG).show()
         }
     }
 
@@ -70,4 +110,5 @@ class MainActivity : AppCompatActivity() {
             )
         }
     }
+
 }
